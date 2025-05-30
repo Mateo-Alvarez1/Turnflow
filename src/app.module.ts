0e78f4efca8652fec873b2fcefb9e10d06359d14
Mateo <mateoalvarez384@gmail.com>
2025-05-30 11:11:49 -0300

    production

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,21 +5,27 @@ import { ConfigModule } from "@nestjs/config";
 import { TurnModule } from "./turn/turn.module";
 import { LocationModule } from "./location/location.module";
 import { TurnHistoryModule } from "./turn-history/turn-history.module";
-import { EventsWsModule } from './events-ws/events-ws.module';
+import { EventsWsModule } from "./events-ws/events-ws.module";
 @Module({
   imports: [
     AuthModule,
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      database: process.env.DB_NAME,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      autoLoadEntities: true,
-      synchronize: true,
+      url: process.env.DB_URL,
+      autoLoadEntities: true, // detecta las entidades automaticamente
+      synchronize: true, // sincroniza nuestras entidades con la DB al inciar el programa
     }),
+    // TypeOrmModule.forRoot({
+    //   type: "postgres",
+    //   host: process.env.DB_HOST,
+    //   port: Number(process.env.DB_PORT),
+    //   database: process.env.DB_NAME,
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
     TurnModule,
     LocationModule,
     TurnHistoryModule,
